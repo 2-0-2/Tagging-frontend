c503b2d900b31ecfa463ac6fa82845e49bfa262e
rkdalswl718 <rkdalswl070718@gmail.com>
2024-06-29 23:08:51 +0900

    ADD :: 레이아웃 완성

--- src/pages/Typing/index.tsx
+++ src/pages/Typing/index.tsx
@@ -7,13 +7,24 @@ const index = () => {
   return (
     <s.Typing_container>
     <s.Typing_layout>
-      <img src={logo} style={{ width: "150px", height: "auto"}}/>
-      <s.Typing_box_layout>
+      <s.LogoImage src={logo} style={{ width: "150px", height: "auto"}}/>
       <s.Typing_box>
+        <s.Typing_section_one>
+          <s.Typing_stats_box>
 
+          </s.Typing_stats_box>
+          <s.Typing_stats_box>
+
+          </s.Typing_stats_box>
+          <s.Typing_stats_box>
+
+          </s.Typing_stats_box>
+          <s.Typing_stats_box>
+
+          </s.Typing_stats_box>
+        </s.Typing_section_one>
 
       </s.Typing_box>
-      </s.Typing_box_layout>
     </s.Typing_layout>
     </s.Typing_container>
   );
--- src/pages/Typing/style.ts
+++ src/pages/Typing/style.ts
@@ -1,28 +1,50 @@
 import styled from "styled-components";
 
-export const Typing_container = styled.div `
+export const Typing_container = styled.div`
   width: 100vw;
   height: 100vh;
-  background: #F6F7FA;
-
-`
-
-export const Typing_layout = styled.div `
-  padding: 5% 200px 0 200px;
-`
-
-export const Typing_box_layout = styled.div`
+  background: #f6f7fa;
   display: flex;
   justify-content: center;
-  margin: 0 auto;
   align-items: center;
-`
+`;
 
-export const Typing_box = styled.div `
-width: 60vw;
-height: 48vh;
-background: #FFFFFF;
-box-shadow: 0px 0px 30px rgba(70, 70, 70, 0.1);
-border-radius: 20px;
+export const Typing_layout = styled.div`
+  padding: 5% 10% 10% 10%;
+  `;
 
-`
+export const LogoImage = styled.img`
+  width: 150px;
+  height: auto;
+  position: absolute;
+  top: 8vh;
+  left: 200px;
+  `;
+
+export const Typing_box = styled.div`
+  width: 60vw;
+  height: 50vh;
+  background: #ffffff;
+  box-shadow: 0px 0px 30px rgba(70, 70, 70, 0.1);
+  border-radius: 20px;
+  `;
+
+export const Typing_section_one = styled.div`
+  display: flex;
+  margin: 0 auto;
+  width: 90%;
+  height: 30%;
+  border-bottom: 1px solid #D8D8D8;
+  align-items: center;
+
+  `
+  
+  export const Typing_stats_box = styled.div`
+  background-color: pink;
+  width: 25%;
+  height: 70%;
+  `
+
+export const Typing_current_score= styled.div`
+
+  `;
