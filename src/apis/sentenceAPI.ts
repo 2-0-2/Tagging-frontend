16a924cd04cf01168fd506b09a01497124d3bd41
rkdalswl718 <rkdalswl070718@gmail.com>
2024-06-30 11:06:13 +0900

    ADD :: api

--- src/apis/sentenceAPI.ts
+++ src/apis/sentenceAPI.ts
@@ -1,1 +1,11 @@
-import axios from "axios";
+import axios from 'axios';
+
+export const fetchSentence = () => {
+  return axios.get('http://211.112.175.88:5050/')
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      console.error('API 호출 중 오류 발생:', error);
+    });
+};
--- src/pages/Typing/index.tsx
+++ src/pages/Typing/index.tsx
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as s from "./style";
 import logo from "../../assets/로고.svg";
+import { fetchSentence } from '../../apis/sentenceAPI';
 
+const TypingPage = () => {
+  const [sentence, setSentence] = useState('');
 
-const index = () => {
+  useEffect(() => {
+    const getSentence = () => {
+      fetchSentence()
+        .then(data => {
+          setSentence(data.sentence);
+        })
+        .catch(error => {
+          console.error('문장 가져오기 실패:', error);
+        });
+    };
+
+    getSentence();
+  }, []);
+
   return (
     <s.Typing_container>
-    <s.Typing_layout>
-      <s.LogoImage src={logo} style={{ width: "150px", height: "auto"}}/>
-      <s.Typing_box>
-        <s.Typing_section_one>
-          <s.Typing_stats_box>
-
-          </s.Typing_stats_box>
-          <s.Typing_stats_box>
-
-          </s.Typing_stats_box>
-          <s.Typing_stats_box>
-
-          </s.Typing_stats_box>
-          <s.Typing_stats_box>
-
-          </s.Typing_stats_box>
-        </s.Typing_section_one>
-
-      </s.Typing_box>
-    </s.Typing_layout>
+      <s.Typing_layout>
+        <s.LogoImage src={logo} style={{ width: "150px", height: "auto" }} />
+              <h1>{sentence}</h1>
+        <s.Typing_box>
+          <s.Typing_section_one>
+            <s.Typing_stats_box>
+            </s.Typing_stats_box>
+            <s.Typing_stats_box>
+            </s.Typing_stats_box>
+            <s.Typing_stats_box>
+            </s.Typing_stats_box>
+          </s.Typing_section_one>
+        </s.Typing_box>
+      </s.Typing_layout>
     </s.Typing_container>
   );
 };
 
-export default index;
+export default TypingPage;
