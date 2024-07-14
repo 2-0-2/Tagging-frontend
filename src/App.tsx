3f4120271f40e9e473190d136002d2135e43e34a
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-07-15 03:56:46 +0900

    Feat : game-result 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import React from "react";
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Home from './pages/Home/index';
-import Typing from './pages/Typing/index';
-import Game from './pages/Game/index';
-
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/index";
+import Typing from "./pages/Typing/index";
+import Game from "./pages/Game/index";
+import GameResult from "./pages/GameResult";
 
 function App() {
+  const [gameScore, setGameScore] = useState<number>(0); // State to hold game score
+
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL} >
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/typing" element={<Typing/>} />
-        <Route path="/game" element={<Game/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/typing" element={<Typing />} />
+        <Route path="/game" element={<Game setGameScore={setGameScore} />} />
+        <Route
+          path="/game-result"
+          element={
+            <GameResult isOpen={true} onClose={() => {}} score={gameScore} />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
