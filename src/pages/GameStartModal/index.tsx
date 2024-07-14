8893ee4fcdd887daf5fdf48b7d78765187dd7934
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-07-15 03:17:18 +0900

    Feat : GameStartModal  퍼블리싱

---
+++ src/pages/GameStartModal/index.tsx
@@ -0,0 +1,21 @@
+import * as S from "./style";
+
+const GameStartModal = () => {
+  return (
+    <S.ModalOverlay>
+      <S.Layout>
+        <S.Title>게임설명</S.Title>
+        <S.SubTitleContainer>
+          <S.SubTitle>
+            1. 같은 색의 영어 단어와 뜻을 입력<div> 하면 +10</div>
+          </S.SubTitle>
+          <S.SubTitle>2. 단어가 땅에 닿으면 생명 -1</S.SubTitle>
+          <S.SubTitle>3. 생명이 모두 없어지면 게임 종료</S.SubTitle>
+        </S.SubTitleContainer>
+        <S.GameStartBtn>게임시작하기</S.GameStartBtn>
+      </S.Layout>
+    </S.ModalOverlay>
+  );
+};
+
+export default GameStartModal;
